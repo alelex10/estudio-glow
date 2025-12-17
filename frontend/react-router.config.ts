ffa1cfd592f6e558b6791b6e3e3e0d801ed2bf8e
alelex10 <elvizvida@gmail.com>
2025-12-17 15:59:22 -0300

    config react router

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   // Config options...
@@ -6,4 +7,5 @@ export default {
   future: {
     v8_middleware: true,
   },
+  presets: [vercelPreset()],
 } satisfies Config;
