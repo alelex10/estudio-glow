7c5cd68a707ee4ed94ce980119008599f55dc4f1
alelex10 <elvizvida@gmail.com>
2025-12-19 16:01:06 -0300

    add env

--- frontend/app/common/config/api.ts
+++ frontend/app/common/config/api.ts
@@ -1,5 +1,5 @@
 // Configuración de la API
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Endpoints
 export const API_ENDPOINTS = {
