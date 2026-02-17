dea729b2c65b9f9cb914b58f228186c80965deb3
alelex10 <elvizvida@gmail.com>
2026-02-17 12:18:53 -0300

    edit promp arg-actions

--- frontend/app/routes/admin/logout.ts
+++ frontend/app/routes/admin/logout.ts
@@ -1,8 +1,9 @@
 import { redirect } from "react-router";
 import { contextProvider, userContext } from "~/common/context/context";
 import { authService } from "~/common/services/authService";
+import type { Route } from "./+types/logout";
 
-export async function action({ request }: { request: Request }) {
+export async function action({ request, }: Route.ActionArgs) {
   try {
     await authService.logout();
 
